cb138cef18157ed8df7753672735a210dbd6e63b
fyodorovv <v.fyodorov@list.ru>
2023-12-31 20:42:53 +0300

    Add files via upload

---
+++ HW6/Task 4/models_4.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text, Boolean
+
+
+# Pydantic
+class TaskIn(BaseModel):
+    title: str = Field()
+    description: str = Field(default=None)
+    done: bool = Field(default=False)
+
+
+class TaskOut(TaskIn):
+    id: int
+
+
+# sqlalchemy models
+Base = declarative_base()
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(Text, nullable=True)
+    done = Column(Boolean, default=False)
